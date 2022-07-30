75f1454cbb713b9084cf244c11b129674525d3ab
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-07-30 15:23:03 +0300

    add rust library

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn add_test() {
+    assert_eq!(1 + 1, add(1, 1));
 }
